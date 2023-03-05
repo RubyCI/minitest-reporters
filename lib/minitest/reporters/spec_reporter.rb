2590a80a4ea9660b97324434621b6ae8a665378b
Nesha <nesha@kolosek.com>
2023-03-05 15:18:53 +0100

    Fixed to location and json

--- lib/minitest/reporters/spec_reporter.rb
+++ lib/minitest/reporters/spec_reporter.rb
@@ -1,3 +1,4 @@
+require 'json'
 module Minitest
   module Reporters
     # Turn-like reporter that reads like a spec.
@@ -92,9 +93,8 @@ module Minitest
 
       def test_finished(test)
         output = get_output
-
-        location = if test.source_location.join(":").start_with?("/app")
-                    test.source_location.join(":")
+        location = if test.location.join(":").start_with?("/app")
+                    test.location.join(":")
                    else
                     if (file = `cat /cache/bundle/minitest_cache_file | grep '\#{test.klass} => '`.split(' => ').last&.chomp)
                       file + ":"
